9c0c327e9bd8a0a0c00f109cb2796b1cb8a88d94
AMAN SINGH <amansingh635217@gmail.com>
2026-07-21 16:26:35 +0530

    POTD

--- Maximize Active Section with Trade I.cpp
+++ Maximize Active Section with Trade I.cpp
@@ -48,3 +48,31 @@ class Solution {
             return cntOnes + maxpairsum-1;
         }
     };
+
+
+// ---------------------------- JAVA CODE ---------------------------------//
+
+class Solution {
+    public int maxActiveSectionsAfterTrade(String s) {
+        s += '1';
+        int curr = 0, cntOnes = 0, maxpairsum = 0, prev = 0, n = s.length();
+        for(int i = 0;i < n;){
+            if(s.charAt(i) == '0') {
+                curr++;
+                i++;
+            }
+            else {
+                if(prev != 0) {
+                    maxpairsum = Math.max(maxpairsum, curr + prev);
+                }
+                prev = curr;
+                curr = 0;
+                while(i < n && s.charAt(i) == '1') {
+                    cntOnes++;
+                    i++;
+                }
+            }
+        }
+        return cntOnes + maxpairsum-1;
+    }
+}
