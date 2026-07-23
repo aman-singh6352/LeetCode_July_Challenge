377ebb1d6d7cfe00f72ffb342f30007f1b3f4be0
AMAN SINGH <amansingh635217@gmail.com>
2026-07-23 11:16:28 +0530

    POTD

--- Number of Unique XOR Triplets I.cpp
+++ Number of Unique XOR Triplets I.cpp
@@ -13,3 +13,21 @@ class Solution {
             return (1 << ++msbidx);
         }
     };
+
+
+/* ---------------------------- JAVA CODE -----------------------------------*/
+
+class Solution {
+    public int uniqueXorTriplets(int[] nums) {
+        int n = nums.length;
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+        int i = 0, msbidx = 0;
+        // 1 -> n
+        while(i < 32){
+            if((n & (1 << i)) != 0) msbidx = i;
+            i++;
+        }
+        return (int)(1 << ++msbidx);
+    }
+}
